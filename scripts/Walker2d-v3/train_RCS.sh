d4641f8fb50a040767cfe2950e850e01e06391c3
lizhuo-1994 <guitar_318576604@yahoo.co.jp>
2022-04-25 12:19:16 +0900

    update

--- scripts/Walker2d-v3/train_RCS.sh
+++ scripts/Walker2d-v3/train_RCS.sh
@@ -1,3 +1,8 @@
+python train.py --policy EMAC --env Walker2d-v3 --k 2 --max_timesteps 400001 --exp_name k2_alpha_01_beta_05 --alpha 0.1 --device cuda:0 --eval_freq 1000 --prioritized --beta 0.5 --seed 0
+python train.py --policy EMAC --env Walker2d-v3 --k 2 --max_timesteps 400001 --exp_name k2_alpha_01_beta_05 --alpha 0.1 --device cuda:0 --eval_freq 1000 --prioritized --beta 0.5 --seed 1
+python train.py --policy EMAC --env Walker2d-v3 --k 2 --max_timesteps 400001 --exp_name k2_alpha_01_beta_05 --alpha 0.1 --device cuda:0 --eval_freq 1000 --prioritized --beta 0.5 --seed 2
+python train.py --policy EMAC --env Walker2d-v3 --k 2 --max_timesteps 400001 --exp_name k2_alpha_01_beta_05 --alpha 0.1 --device cuda:0 --eval_freq 1000 --prioritized --beta 0.5 --seed 3
+python train.py --policy EMAC --env Walker2d-v3 --k 2 --max_timesteps 400001 --exp_name k2_alpha_01_beta_05 --alpha 0.1 --device cuda:0 --eval_freq 1000 --prioritized --beta 0.5 --seed 4
 python train.py --policy RCS --env Walker2d-v3 --k 2 --max_timesteps 400001 --exp_name k2_alpha_01_beta_05_step_1 --alpha 0.1 --device cuda:0 --eval_freq 1000 --prioritized --beta 0.5 --seed 0 --order 1 --grid_num 5 --decay 0.2 --mode state_action
 python train.py --policy RCS --env Walker2d-v3 --k 2 --max_timesteps 400001 --exp_name k2_alpha_01_beta_05_step_1 --alpha 0.1 --device cuda:0 --eval_freq 1000 --prioritized --beta 0.5 --seed 1 --order 1 --grid_num 5 --decay 0.2 --mode state_action
 python train.py --policy RCS --env Walker2d-v3 --k 2 --max_timesteps 400001 --exp_name k2_alpha_01_beta_05_step_1 --alpha 0.1 --device cuda:0 --eval_freq 1000 --prioritized --beta 0.5 --seed 2 --order 1 --grid_num 5 --decay 0.2 --mode state_action
